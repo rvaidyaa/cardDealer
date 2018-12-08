d8483e88a504458f48f890e1e744d4feffa6c843
Rahul Vaidya <rvaidyaa@gmail.com>
2018-12-08 14:12:46 -0700

    added Deck constructor

--- routes/index.js
+++ routes/index.js
@@ -1,68 +1,37 @@
 var express = require("express");
 var router = express.Router();
 
-let ordered = [
-  "2-s",
-  "3-s",
-  "4-s",
-  "5-s",
-  "6-s",
-  "7-s",
-  "8-s",
-  "9-s",
-  "T-s",
-  "J-s",
-  "Q-s",
-  "K-s",
-  "A-s",
-  "2-h",
-  "3-h",
-  "4-h",
-  "5-h",
-  "6-h",
-  "7-h",
-  "8-h",
-  "9-h",
-  "T-h",
-  "J-h",
-  "Q-h",
-  "K-h",
-  "A-h",
-  "2-c",
-  "3-c",
-  "4-c",
-  "5-c",
-  "6-c",
-  "7-c",
-  "8-c",
-  "9-c",
-  "T-c",
-  "J-c",
-  "Q-c",
-  "K-c",
-  "A-c",
-  "2-d",
-  "3-d",
-  "4-d",
-  "5-d",
-  "6-d",
-  "7-d",
-  "8-d",
-  "9-d",
-  "T-d",
-  "J-d",
-  "Q-d",
-  "K-d",
-  "A-d"
-];
+
+
+class Deck {
+  constructor(){
+    this.deck =[];
+
+    const suites = ['Spades','Hearts','Clubs','Diamonds'];
+    const values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+
+    for (let suit in suites){
+      for(let value in values){
+        this.deck.push(`${values[value]} of ${suites[suit]}`);
+      }
+    }
+  }
+};
+const default_order = new Deck();
+
 
 let discard = [];
 router.get("/", async (req, res) =>{
+  res.status(200).json({default_order});
+
+});
+router.get("/shuffle", async (req, res) =>{
   res.status(200).json({ordered});
 });
 router.post("/discard/:card", async (req, res) => {
   let discard = req.params.card;
-  discard.push
+  discard.push(discard);
+
 
 });
 module.exports = router;
