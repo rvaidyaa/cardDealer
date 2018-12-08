2475eedd9caa7d63b041ee8ee9a5659e6a4f69b6
Rahul Vaidya <rvaidyaa@gmail.com>
2018-12-08 15:24:00 -0700

    added deal function

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,67 @@
 var express = require("express");
 var router = express.Router();
 
-
-
 class Deck {
-  constructor(){
-    this.deck =[];
+  constructor() {
+    this.deck = [];
+    let discard = [];
 
-    const suites = ['Spades','Hearts','Clubs','Diamonds'];
-    const values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+    const suites = ["Spades", "Hearts", "Clubs", "Diamonds"];
+    const values = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King",
+      "Ace"
+    ];
 
-    for (let suit in suites){
-      for(let value in values){
+    for (let suit in suites) {
+      for (let value in values) {
         this.deck.push(`${values[value]}-of-${suites[suit]}`);
       }
     }
   }
   shuffle() {
-    const {deck} = this;
-    let m = deck.length, i;
-    
+    const { deck } = this;
+    let m = deck.length,
+      i;
+
     while (m) {
-      i = Math.floor(Math.random()* m--);
-      [deck[m],deck[i]] = [deck[i],deck[m]]
+      i = Math.floor(Math.random() * m--);
+      [deck[m], deck[i]] = [deck[i], deck[m]];
     }
     return this;
   }
-  deal(){
-    return this.deck.pop();
+  deal() {
+    return this.deck.shift();
   }
-};
-const default_order = new Deck();
-
-
+}
+const deck = new Deck();
 
 let discard = [];
-router.get("/", async (req, res) =>{
-  res.status(200).json({default_order});
-
+router.get("/", async (req, res) => {
+  res.status(200).json({ deck });
 });
-router.get("/shuffle", async (req, res) =>{
-  const shuffled = default_order.shuffle();
-  res.status(200).json({shuffled});
+router.get("/shuffle", async (req, res) => {
+  deck.shuffle();
+  res.status(200).json({ deck });
+});
+router.get("/deal", async (req, res) => {
+  deck.deal()
+  res.status(200).json({deck});
 });
 router.post("/discard/:card", async (req, res) => {
   let discard_card = req.params.card;
   discard.push(discard_card);
 
-  res.json({discard});
-
-
+  res.json({ discard });
 });
 module.exports = router;
